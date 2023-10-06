b4f58497bb9b34e2b0c77c9b1033355d2795d3db
BiahMarqyes <santosmarquesbeatriz@gmail.com>
2023-10-06 11:47:21 -0300

    eee

--- confeitaria/src/pages/Menu/index.tsx
+++ confeitaria/src/pages/Menu/index.tsx
@@ -1,3 +1,29 @@
-export default function Menu(){
-    return
+import styles from './Cardapio.module.scss'
+import Logo from '../../assets/cardapio/logo.jpg'
+import Buscador from './Buscador';
+import React, { useState } from 'react';
+import Filtros from './Filtros';
+
+export default function Cardapio(){
+  const [buscar, setBusca] = useState("");
+    return (
+        <main>
+            <nav className={ styles.menu }>
+            <img src={Logo} alt="Logo do Restaurante"/>
+            {/*Cardapio da Melhor Confeitaria*/}
+            </nav>
+            <header className={ styles.header }>
+                <div className= { styles.header__text }>
+                  "A confeitaria que adoça sua vida!"
+                </div>
+            </header>
+            <section className={styles.cardapio}>
+              <h3 className={styles.cardapio__titulo} >cardapio</h3>
+              <Buscador busca={buscar} setBusca={setBusca}/>
+              <div className={styles.cardapio__filtros}>
+                <Filtros/>
+              </div>
+            </section>
+        </main>
+    );
 }
